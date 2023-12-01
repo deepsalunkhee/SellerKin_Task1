c7e09155137ee84c110c97daf2bcf1ede30abedb
deepsalunkhee <deepsalunkhee@gmail.com>
2023-12-01 19:03:53 +0530

    deploying

--- client/src/components/Cards.js
+++ client/src/components/Cards.js
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import './Cards.css'; // Import the CSS file
+import './Cards.css'; 
 
 const Card = ({ listing }) => {
   const [images, setImages] = useState([]);
   const[revenue,setRevenue]=useState([]);
 
-  // Function to truncate text if it exceeds a certain length
+  
   const truncateText = (text, maxLength) => {
     if (text.length > maxLength) {
-      return text.substring(0, maxLength - 3) + '...'; // Truncate text and add ellipsis
+      return text.substring(0, maxLength - 3) + '...'; 
     }
     return text;
   };
@@ -19,7 +19,7 @@ const Card = ({ listing }) => {
         const response = await fetch(`http://localhost:3000/images?id=${listing.listing_id}`);
         if (response.ok) {
           const result = await response.json();
-          setImages(result.results || []); // Set the fetched data to the state variable
+          setImages(result.results || []); 
         } else {
           console.error('Failed to fetch data');
         }
@@ -37,7 +37,8 @@ const Card = ({ listing }) => {
         const response = await fetch(`http://localhost:3000/economic?id=${listing.listing_id}`);
         if (response.ok) {
           const result = await response.json();
-          setRevenue(result.results || []); // Set the fetched data to the state variable
+          setRevenue(result || []);
+           console.log(revenue.price.amount);
         } else {
           console.error('Failed to fetch data');
         }
@@ -56,10 +57,9 @@ const Card = ({ listing }) => {
       {images.length > 0 && (
         <img src={images[0].url_170x135} alt={listing.title} />
       )}
-      <h3>{truncateText(listing.title, 30)}</h3> {/* Limit the name to 30 characters */}
-      <p>Description: {truncateText(listing.description, 100)}</p> {/* Limit the description to 100 characters */}
-      <p>Sales:XXXXXX</p>
-      <p>Revenue:XXXXXXXXX</p>
+      <h3>{truncateText(listing.title, 30)}</h3>
+      <p>Description: {truncateText(listing.description, 100)}</p> 
+      <p>Sales:{Math.ceil(revenue.views*0.0399 +  revenue.num_favorers*0.175)}</p>
     </div>
   );
 };
--- client/src/pages/Products.js
+++ client/src/pages/Products.js
@@ -1,21 +1,19 @@
-// Products.js
-
 import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import Cards from '../components/Cards';
-import './Product.css'; // Import the CSS file
+import './Product.css'; 
 
 const Products = () => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    // Fetch data from your backend endpoint when the component mounts
+   
     const fetchData = async () => {
       try {
         const response = await fetch('http://localhost:3000/list');
         if (response.ok) {
           const result = await response.json();
-          setData(result.results || []); // Set the fetched data to the state variable
+          setData(result.results || []); 
         } else {
           console.error('Failed to fetch data');
         }
@@ -31,7 +29,6 @@ const Products = () => {
     <div>
       <Header />
       <div className="card-container">
-        {/* Map through 'data' and render a Card component for each listing */}
         {data.map((listing, index) => (
           <Cards key={index} listing={listing} />
         ))}
