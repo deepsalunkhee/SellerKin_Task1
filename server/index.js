9c88ddbf7f51ae7337a2748dbffe4d24e0ca83de
deepsalunkhee <deepsalunkhee@gmail.com>
2023-12-02 11:07:27 +0530

    deploying1.6

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ async function startServer() {
     let tokenData = "";
 
     const port = 3000;
-    const baseurl = `seller-kin-task1-server.vercel.app`;
+    const baseurl = `https://seller-kin-task1-server.vercel.app`;
     // const baseurl = `http://localhost:${port}`;
     dotenv.config();
     const apitoken = process.env.API_TOKEN;
